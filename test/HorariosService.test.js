15460b53f1a5f6e3284e3dded1d97355577d8734
Edras <edrassimoes2@hotmail.com>
2023-10-08 15:26:10 -0300

    Upload HorariosService tests

---
+++ test/HorariosService.test.js
@@ -0,0 +1,75 @@
+import {describe, expect, it} from "vitest";
+import {HorariosService} from "./mock/HorariosServiceMock.js";
+
+describe('HorariosService Test', () => {
+
+    it('buscaPorNome Test', async () => {
+        const service = new HorariosService()
+        const result = await service.buscaPorNome('chris')
+        expect(result).toEqual(JSON.stringify(
+            [{
+                    "nomeDoProfessor": "chris",
+                    "horarioDeAtendimento": "19:30",
+                    "periodo": "noturno",
+                    "sala": 1,
+                    "predio": ["1", "2", "3", "4", "5"]
+                },
+                {
+                    "nomeDoProfessor": "chris",
+                    "horarioDeAtendimento": "15:30",
+                    "periodo": "integral",
+                    "sala": 7,
+                    "predio": ["1", "2", "3", "4", "5"]
+                }]
+        ))
+    });
+
+    it('buscaTodos Test', async () => {
+        const service = new HorariosService()
+        const result = await service.buscaTodos()
+        expect(result).toEqual(JSON.stringify([
+            {
+                nomeDoProfessor: "chris",
+                horarioDeAtendimento: "19:30",
+                periodo: "noturno",
+                sala: 1,
+                predio: ['1', '2', '3', '4', '5']
+            },
+            {
+                nomeDoProfessor: "chris",
+                horarioDeAtendimento: "15:30",
+                periodo: "integral",
+                sala: 7,
+                predio: ['1', '2', '3', '4', '5']
+            },
+            {
+                nomeDoProfessor: "renzo",
+                horarioDeAtendimento: "21:30",
+                periodo: "noturno",
+                sala: 12,
+                predio: ['1', '2', '3', '4', '5']
+            },
+            {
+                nomeDoProfessor: "renzo",
+                horarioDeAtendimento: "13:30",
+                periodo: "integral",
+                sala: 16,
+                predio: ['1', '2', '3', '4', '5']
+            },
+            {
+                nomeDoProfessor: "marcelo",
+                horarioDeAtendimento: "19:30",
+                periodo: "noturno",
+                sala: 22,
+                predio: ['1', '2', '3', '4', '5']
+            },
+            {
+                nomeDoProfessor: "marcelo",
+                horarioDeAtendimento: "15:30",
+                periodo: "integral",
+                sala: 3,
+                predio: ['1', '2', '3', '4', '5']
+            },
+        ]))
+    });
+});
--- test/mock/HorariosServiceMock.js
+++ test/mock/HorariosServiceMock.js
@@ -1,10 +1,11 @@
-import { jsonString } from "./HorariosConst"
+import {jsonString} from "./HorariosConst"
+
 export class HorariosService {
     async buscaPorNome(nome) {
         return new Promise((resolve, reject) => {
-            // SIMULAÇÂO DE REQUISIÇÂO PRO SERVICO
             setTimeout(() => {
-                const dados = getDados().filter(horario => horario.nomeDoProfessor === nome)
+                // Mock da função getDados()
+                const dados = jsonString.filter(horario => horario.nomeDoProfessor === nome)
                 if (!dados.length)
                     return reject("Professor não cadastrado")
                 return resolve(JSON.stringify(dados))
@@ -14,8 +15,8 @@ export class HorariosService {
 
     async buscaTodos() {
         return new Promise((resolve, reject) => {
-            // SIMULAÇÂO DE REQUISIÇÂO PRO SERVICO
             setTimeout(() => {
+                // Mock da função getDados()
                 resolve(JSON.stringify(jsonString))
             }, 1000)
         })
